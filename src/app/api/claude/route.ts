8efe3aa9be86b19cfecb808cc7c28c0c42425b70
Woohyeok Baek <qordngur156@gmail.com>
2024-05-06 12:09:51 +0900

    Feat: 스토리 생성 프롬프트 작성

--- src/app/api/claude/route.ts
+++ src/app/api/claude/route.ts
@@ -8,7 +8,26 @@ export async function POST(req: NextRequest) {
   const message = await anthropic.messages.create({
     model: 'claude-3-haiku-20240307',
     max_tokens: 1024,
-    messages: [{ role: 'user', content: formData.myText }],
+    messages: [{ role: 'user', content: `장르: ${formData.genre}, 배경: ${formData.background}` }],
+    system: `너는 창의적인 스토리 도입부를 작성하는 스토리텔러야.
+
+    [ 작업순서 ]
+    - 유저가 제공한 장르와 배경을 바탕으로 스토리 도입부를 작성해줘
+    - 스토리 도입부 작성이 끝나면 이것을 바탕으로 선택지 항목들을 작성해줘
+    
+    [ 규칙 ]
+    - 스토리 도입부는 장르 및 배경을 고려해서 작성해줘
+    - 스토리 도입부는 1인칭 시점으로 작성해줘
+    - 스토리 도입부는 장르 및 배경에 따라 적합한 문체로 작성해줘
+    - 스토리 도입부에는 장르 및 배경에 따른 분위기와 세계관 설명을 창의적으로 표현해줘
+    - 스토리 도입부 마지막에는 다음 내용을 전개하기 위한 내용으로 작성해줘
+    - 스토리 도입부는 자세하게 작성해줘
+    - 스토리 도입부 작성이 끝나면 선택지 항목들을 작성해줘
+    - 선택지 항목은 각각 번호를 붙여줘
+    - 선택지 항목은 장르 및 배경, 스토리 도입부를 바탕으로 작성해줘
+    - 선택지 항목은 상황에 따라 행동, 결정, 대사 등등 스토리 전개를 위한 요소들이 포함되어야해
+    - 선택지 항목은 랜덤으로 최소 2개에서 최대4개 사이로 작성해줘
+    - 스토리 도입부와 선택지 항목들 외에 불필요한 문장은 적지말아줘`,
   });
   console.log(message);
   return Response.json({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,42 @@ import axios from 'axios';
 import { useState } from 'react';
 
 export default function Home() {
-  const [myText, setMyText] = useState('');
+  const [genre, setGenre] = useState('');
+  const [background, setBackground] = useState('');
   const [claudeText, setClaudeText] = useState('');
 
   const post = async () => {
-    const res = await axios.post('/api/claude', { myText });
+    if (!genre || !background) {
+      console.log('??');
+      return;
+    }
+    const res = await axios.post('/api/claude', { genre, background });
     console.log(res.data.data);
-    setMyText('');
     setClaudeText(res.data.data);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       메인페이지
-      <div className="p-5 border-white min-h-10 min-w-10">
+      <div className="flex flex-col gap-5">
+        <select value={genre} onChange={e => setGenre(e.target.value)}>
+          <option>추리</option>
+          <option>공포</option>
+          <option>모험</option>
+          <option>생존</option>
+        </select>
+        <select value={background} onChange={e => setBackground(e.target.value)}>
+          <option>중세</option>
+          <option>판타지</option>
+          <option>아포칼립스</option>
+          <option>현대</option>
+          <option>SF</option>
+        </select>
+      </div>
+      <div className="p-5 min-h-10 min-w-10">
         <div>claud 답변</div>
         <div>{claudeText}</div>
       </div>
-      <input type="text" value={myText} onChange={e => setMyText(e.target.value)} className="text-black" />
       <button onClick={post}>click</button>
     </main>
   );
