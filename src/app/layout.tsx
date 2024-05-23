345f4b9d2be9149527be191d7ea523d9f8000902
Woohyeok Baek <qordngur156@gmail.com>
2024-05-23 14:50:15 +0900

    Chore: 로그인 페이지 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <RootProvider>
           <main className="max-w-6xl min-h-screen mx-auto">
             <NavigationBar />
-            {children}
+            <div className="flex justify-center py-16">{children}</div>
           </main>
         </RootProvider>
         <div id="overlay" />
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,12 @@ import { Text } from '@/components/shared/Text';
 
 export default function LoginPage() {
   return (
-    <main>
-      <div className="flex flex-col items-center overflow-hidden bg-white rounded-lg dark:bg-gray-800 lg:max-w-4xl">
-        <div className="w-full px-6 py-8">
-          <Text align="center" size="xl">
-            Welcome back!
-          </Text>
-          <LoginButton />
-        </div>
+    <main className="flex flex-col items-center overflow-hidden bg-white rounded-lg dark:bg-gray-800">
+      <div className="w-full px-6 py-8">
+        <Text align="center" size="xl">
+          Welcome back!
+        </Text>
+        <LoginButton />
       </div>
     </main>
   );
