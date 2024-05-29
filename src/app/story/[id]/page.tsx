dc08e39115e38a5ddfb35cabc4f5f7b865a7ef76
Woohyeok Baek <qordngur156@gmail.com>
2024-05-29 15:23:41 +0900

    Chore: 스토리 페이지 레이아웃 수정

--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -28,36 +28,10 @@ export default async function StoryPage({ params }: ScenarioPlayProps) {
   }
 
   return (
-    <main>
+    <main className="max-w-2xl">
       <HydrationBoundary state={dehydrate(queryClient)}>
         <StoryAdvancer storyId={params.id} />
       </HydrationBoundary>
     </main>
   );
 }
-
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-// import { redirect } from 'next/navigation';
-// // components
-// import StoryAdvancer from '@/components/story/StoryAdvancer';
-// // remotes
-// import { getStoryById } from '@/remotes/mongodb/server/story';
-
-// interface ScenarioPlayProps {
-//   params: { id: string };
-// }
-// export default async function StoryPage({ params }: ScenarioPlayProps) {
-//   const session = await getServerSession(authOptions);
-//   const story = await getStoryById(params.id);
-
-//   if (!session?.user && session?.user.id !== story.userId) {
-//     return redirect('/login');
-//   }
-
-//   return (
-//     <main>
-//       <StoryAdvancer story={story} />
-//     </main>
-//   );
-// }
