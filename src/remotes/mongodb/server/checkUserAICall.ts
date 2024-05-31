ee9c34bdb84a1dfa83e654e4adc0ff89d5d3abde
Woohyeok Baek <qordngur156@gmail.com>
2024-05-31 18:58:46 +0900

    Refactor: AI api call 횟수 체크 조건문 로직 변경

--- src/remotes/mongodb/server/checkUserAICall.ts
+++ src/remotes/mongodb/server/checkUserAICall.ts
@@ -8,9 +8,9 @@ import { UserAICallSchema } from '@/remotes/schema';
 export const checkUserAICall = async (): Promise<{ isValid: boolean; response?: NextResponse }> => {
   const session = await getServerSession(authOptions);
 
+  // 로그인 정보 확인
   if (!session) {
-    const response = NextResponse.json({ message: '로그인 정보가 없습니다.' }, { status: 401 });
-    return { isValid: false, response: response };
+    return { isValid: false, response: NextResponse.json({ message: '로그인 정보가 없습니다.' }, { status: 401 }) };
   }
 
   const db = (await connectDB).db('prototype');
@@ -18,28 +18,37 @@ export const checkUserAICall = async (): Promise<{ isValid: boolean; response?: 
   const currentDate = new Date().toISOString().split('T')[0]; //2024-05-31
 
   try {
-    if (result) {
-      const userCall = UserAICallSchema.parse(result);
-      if (userCall.lastCallDate === currentDate) {
-        if (userCall.todayCount >= 2) {
-          const response = NextResponse.json({ message: '일일 횟수가 초과되었습니다.' }, { status: 429 });
-          return { isValid: false, response: response };
-        } else {
-          await db.collection('userAICall').updateOne({ userId: userCall.userId }, { $inc: { todayCount: 1 } });
-        }
-      } else {
-        await db
-          .collection('userAICall')
-          .updateOne({ userId: userCall.userId }, { $set: { todayCount: 1, lastCallDate: currentDate } });
-      }
-    } else {
+    // AI Call이 처음인 경우, 새로 생성
+    if (!result) {
+      await db.collection('userAICall').insertOne({
+        userId: session.user.id,
+        todayCount: 1,
+        lastCallDate: currentDate,
+      });
+      return { isValid: true };
+    }
+
+    const userCall = UserAICallSchema.parse(result);
+    // 오늘 첫 AI Call인 경우, 날짜 초기화
+    if (userCall.lastCallDate !== currentDate) {
       await db
         .collection('userAICall')
-        .insertOne({ userId: session.user.id, todayCount: 1, lastCallDate: currentDate });
+        .updateOne({ userId: userCall.userId }, { $set: { todayCount: 1, lastCallDate: currentDate } });
+      return { isValid: true };
     }
+
+    // 오늘 AI Call todayCount가 초과된 경우
+    if (userCall.todayCount >= 2) {
+      return {
+        isValid: false,
+        response: NextResponse.json({ message: '일일 횟수가 초과되었습니다.' }, { status: 429 }),
+      };
+    }
+
+    // AI Call, todayCount 1증가
+    await db.collection('userAICall').updateOne({ userId: userCall.userId }, { $inc: { todayCount: 1 } });
     return { isValid: true };
-  } catch (error) {
-    const response = NextResponse.json({ message: '서버에러 발생' }, { status: 501 });
-    return { isValid: false, response: response };
+  } catch (err) {
+    return { isValid: false, response: NextResponse.json({ message: '서버에러 발생' }, { status: 501 }) };
   }
 };
