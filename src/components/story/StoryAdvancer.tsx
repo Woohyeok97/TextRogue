6cf59f9f7a984711bff1d2a7047ada7b352ec456
Woohyeok Baek <qordngur156@gmail.com>
2024-06-17 13:51:52 +0900

    Chore: 쿼리옵션 수정

--- src/components/story/StoryAdvancer.tsx
+++ src/components/story/StoryAdvancer.tsx
@@ -7,7 +7,6 @@ import { StoryType } from '@/models';
 // hooks
 import useContinueStory from '@/hooks/useContinueStory';
 import { getStoryById } from '@/remotes/mongodb/server/story';
-import { useState } from 'react';
 
 interface StoryAdvancerProps {
   storyId: string;
@@ -18,17 +17,17 @@ export default function StoryAdvancer({ storyId }: StoryAdvancerProps) {
     queryFn: () => getStoryById(storyId),
     staleTime: Infinity,
   });
+
   const continueStory = useContinueStory({ story } as { story: StoryType });
-  const [test, set] = useState('');
   // 스토리 진행 핸들러
   const handleClick = (choice: string) => {
     continueStory.mutate(choice);
   };
+
   console.log('storyadvacer');
 
   return (
     <div className="flex flex-col justify-between gap-10">
-      <button onClick={() => set(prev => prev + 'ㅋ')}>click</button>
       {story?.log.map((item, i) => <StoryFormat key={i} story={item} onClick={handleClick} />)}
     </div>
   );
