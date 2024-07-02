20584f2a0229227bb4912510ecf04bb9b9a0e8b0
Woohyeok Baek <qordngur156@gmail.com>
2024-07-02 18:18:32 +0900

    Chore: next.config optimizePackageImports 추가

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode : true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+    ]
+  },
+};
+// export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
+
+module.exports = {
+  experimental: {
+    optimizePackageImports: [
+      '@anthropic-ai/sdk',
+      '@hookform/resolvers',
+      '@tanstack/react-query',
+      'axios', 'next-auth',
+      'react-hook-form',
+      'next',
+      'react',
+      'react-dom',
+      'zod'
+    ],
+  },
+}
