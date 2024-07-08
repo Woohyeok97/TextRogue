1dc50a3e47f7240c1ae8963f6461a71b0eb9e52f
Woohyeok Baek <qordngur156@gmail.com>
2024-07-08 14:07:58 +0900

    Feat: Next Image remotePatterns 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode : false,
+  reactStrictMode : true,
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
     ]
   },
 };
 
 export default nextConfig;
+
+// optimizePackageImports 패키지
+// module.exports = {
+//   experimental: {
+//     optimizePackageImports: [
+//       '@anthropic-ai/sdk',
+//       '@hookform/resolvers',
+//       '@tanstack/react-query',
+//       'axios',
+//       'next-auth',
+//       'react-hook-form',
+//       'next',
+//       'react',
+//       'react-dom',
+//       'zod'
+//     ],
+//   },
+// }
