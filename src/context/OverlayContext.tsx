48af56088f065440bbd332a6d6588c249ada6d0a
Woohyeok Baek <qordngur156@gmail.com>
2024-07-13 19:11:19 +0900

    Feat: 오버레이 함수 메모이징 - 리렌더링 최소화

--- src/context/OverlayContext.tsx
+++ src/context/OverlayContext.tsx
@@ -36,6 +36,7 @@ export function OverlayContextProvider({ children }: ProviderProps) {
     (e: MouseEvent) => {
       const target = e.target as Node;
       if (overlayRef.current && !overlayRef.current.contains(target)) {
+        console.log('out!');
         close();
       }
     },
--- src/hooks/useOver.tsx
+++ src/hooks/useOver.tsx
@@ -1,27 +1,28 @@
-import { overlayState } from '@/recoil/atom';
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilCallback } from 'recoil';
+import { overlayState } from '@/recoil/atom';
 
 export default function useOverlay() {
-  const setOverlayState = useSetRecoilState(overlayState);
-
-  // 오버레이 열기
-  const open = (content: React.ReactNode) => {
-    setOverlayState({
-      isOpen: true,
-      content: content,
-    });
-  };
+  // 오버레이 열기 : open 전, 기존 오버레이 isOpen을 확인해서 OverlayProvider(overlayState 변경에 따라 리렌더링 되는 컴포넌트) 리렌더링 최소화
+  const open = useRecoilCallback(({ set, snapshot }) => async (content: React.ReactNode) => {
+    const current = await snapshot.getPromise(overlayState);
+    if (!current.isOpen) {
+      set(overlayState, { isOpen: true, content: content });
+    }
+  });
 
   // 오버레이 닫기
-  const close = () => {
-    setOverlayState({ isOpen: false });
-  };
+  const close = useRecoilCallback(({ reset, snapshot }) => async () => {
+    const current = await snapshot.getPromise(overlayState);
+    if (current.isOpen) {
+      reset(overlayState);
+    }
+  });
 
   // umount 시, 오버레이 닫기
   useEffect(() => {
     return () => {
-      setOverlayState({ isOpen: false });
+      close();
     };
   }, []);
 
